aa22d1a44a217860421d77d4d426a554388888b5
Rahul Singh <rahulsingh52203@gmail.com>
2025-12-31 15:16:44 +0530

    Add files via upload

---
+++ LOGICS/ArrayString_Plaindrom.pl
@@ -0,0 +1,21 @@
+my @strings = ('RADAR', 'RAT', 'MADAM');
+
+for (my $i = 0; $i < @strings; $i++) {
+    my $str = $strings[$i];
+    my $len = length($str);
+    my $is_palindrome = 1;
+    for (my $j = 0; $j < $len / 2; $j++) {
+        my $left_char = substr($str, $j, 1);
+        my $right_char = substr($str, $len - 1 - $j, 1);
+        if (lc($left_char) ne lc($right_char)) {
+            $is_palindrome = 0;
+            last;
+        }
+    }
+    if ($is_palindrome) {
+        print "$str is a palindrome.\n";
+    } else {
+        print "$str is not a palindrome.\n";
+    }
+}
+
