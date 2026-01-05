22a10493df52afa343738aa8c9a4abc805600ad7
Rahul Singh <rahulsingh52203@gmail.com>
2026-01-06 00:13:31 +0530

    Add files via upload

---
+++ LOGICS/Array_rotation1.pl
@@ -0,0 +1,25 @@
+use strict;
+use warnings;
+
+my @arr = (1, 2, 3, 4, 5, 6, 7);
+
+my $k = 4;
+my $n = scalar @arr;
+
+for (my ($start, $end) = (0, $n - 1); $start < $end; $start++, $end--) {
+    
+    my $temp = $arr[$start];
+    $arr[$start] = $arr[$end];
+    $arr[$end] = $temp;
+}
+for (my ($start, $end) = (0, $k - 1); $start < $end; $start++, $end--) {
+    my $temp = $arr[$start];
+    $arr[$start] = $arr[$end];
+    $arr[$end] = $temp;
+}
+for (my ($start, $end) = ($k, $n - 1); $start < $end; $start++, $end--) {
+    my $temp = $arr[$start];
+    $arr[$start] = $arr[$end];
+    $arr[$end] = $temp;
+}
+print "AFTER ROTAION: ", join( " ", @arr), "\n";
